f79a92119cf14ae7632dbe2263d20b4237e09a34
Danil <danilsmy.edu@gmail.com>
2021-10-18 02:13:38 +0300

    Add distribution of records to blocks

--- denseIndexFiles/denseIndexFiles.cpp
+++ denseIndexFiles/denseIndexFiles.cpp
@@ -1,25 +1,45 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <string>
 #include <iomanip>
 #include <ctime>
 using namespace std;
 
 void createMainFile(int number) {
-	ofstream outFile("main.csv");
+	ofstream outFile("main.txt");
 	for (int i = 0; i < number; i++) {
 		string value = "";
 		for (int j = 0; j < 10; j++) {
-			cout << 25 + rand() % 65 << endl;
 			value += char(65 + rand() % 25);
 		}
-		outFile << i << "," << value << 0 << endl;
+		outFile << i << "," << value << "," << 1 << endl;
 	}
 	outFile.close();
 }
-
+void indexFile() {
+	ifstream inFile("main.txt");
+	string temp;
+	int number = 0;
+	while (inFile >> temp) {
+		cout << temp << endl;
+		int posComa = temp.find(',');
+		int key = stoi(temp.substr(0,posComa));
+		temp.erase(0, posComa+1);
+		posComa = temp.find(',');
+		string value = temp.substr(0, posComa);
+		temp.erase(0, posComa + 1);
+		bool isNotDeleted = stoi(temp);
+		string tempNameFile = to_string(key) + ".txt";
+		ofstream outFile(tempNameFile);
+		outFile << key << "," << number << endl;
+		number++;
+	}
+	inFile.close();
+}
 int main()
 {
 	srand(time(NULL));
 	createMainFile(10);
+	indexFile();
 }
 
