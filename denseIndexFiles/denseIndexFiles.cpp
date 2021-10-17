963611af51bda3a3217d91d8f8ca64969fa4683d
Danil <danilsmy.edu@gmail.com>
2021-10-18 01:38:48 +0300

    Add main file generator

--- denseIndexFiles/denseIndexFiles.cpp
+++ denseIndexFiles/denseIndexFiles.cpp
@@ -1,7 +1,25 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+
+void createMainFile(int number) {
+	ofstream outFile("main.csv");
+	for (int i = 0; i < number; i++) {
+		string value = "";
+		for (int j = 0; j < 10; j++) {
+			cout << 25 + rand() % 65 << endl;
+			value += char(65 + rand() % 25);
+		}
+		outFile << i << "," << value << endl;
+	}
+	outFile.close();
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	srand(time(NULL));
+	createMainFile(10);
 }
 
