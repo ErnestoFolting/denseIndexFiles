a3c7e3a45a24419da5524894ed012d41a3be2121
Danil <danilsmy.edu@gmail.com>
2021-10-20 00:22:14 +0300

    Add easy UI and some basic functions

--- denseIndexFiles/denseIndexFiles.cpp
+++ denseIndexFiles/denseIndexFiles.cpp
@@ -5,8 +5,48 @@
 #include <ctime>
 #include <vector>
 using namespace std;
-int numberValues = 11;
-int numberInBlock = 1;
+int numberValues = 90;
+int numberInBlock = 10;
+
+void inputKey() {
+	cout << "Input the data you want to add " << endl;
+	string data;
+	cin >> data;
+}
+void deleteKey() {
+	cout << "Input the key you want to delete" << endl;
+	string data;
+	cin >> data;
+
+}
+void findKey() {
+	cout << "Input the key you want to find" << endl;
+	string data;
+	cin >> data;
+
+}
+void printFiles() {
+	ifstream inFile1("index.txt");
+	ifstream inFile2("main.txt");
+	ifstream inFile3("overflow.txt");
+	string temp;
+	cout << "-----------------------Main area:-----------------------" << endl;
+	while (inFile1 >> temp) {
+		cout << temp << endl;
+	}
+	inFile1.close();
+	cout << "-----------------------Index area:-----------------------" << endl;
+	while (inFile2 >> temp) {
+		cout << temp << endl;
+	}
+	inFile2.close();
+	cout << "-----------------------Overflow area:-----------------------" << endl;
+	while (inFile3 >> temp) {
+		cout << temp << endl;
+	}
+	inFile3.close();
+
+}
 void createMainFile(int number) {
 	ofstream outFile("main.txt");
 	for (int i = 0; i < number; i++) {
@@ -27,7 +67,6 @@ void indexFile() {
 	vector<vector<string>> blocks(10);
 	vector<string> overflow;
 	while (inFile >> temp) {
-		cout << temp << endl;
 		int posComa = temp.find(',');
 		int key = stoi(temp.substr(0,posComa));
 		temp.erase(0, posComa+1);
@@ -51,10 +90,10 @@ void indexFile() {
 		for (int j = 0; j < blocks[i].size(); j++) {
 			outFile << blocks[i][j] << endl;
 		}
-		outFile << endl;
+		outFile << "-" << endl;
 	}
 	outFile.close();
-	ofstream outFileOver("overflow.txt", ofstream::app);
+	ofstream outFileOver("overflow.txt");
 	for (int i = 0; i < overflow.size();i++) {
 		outFileOver << overflow[i] << endl;
 	}
@@ -62,8 +101,31 @@ void indexFile() {
 }
 int main()
 {
-	srand(time(NULL));
 	createMainFile(numberValues);
 	indexFile();
+	printFiles();
+	int res;
+	int outRes = 1;
+	while (outRes != 0) {
+		cout << "Whad do you want to do? \n 1 - input key\n 2 - delete key \n 3 - find key" << endl;
+		cin >> res;
+		switch (res) {
+		case 1:
+			inputKey();
+			break;
+		case 2:
+			deleteKey();
+			break;
+		case 3:
+			findKey();
+			break;
+		default:
+			cout << "Unknown" << endl;
+		}
+		cout << "Do you want to continue? \n 1 - yes \n 0 - no" << endl;
+		cin >> outRes;
+	}
+	
+
 }
 
